5949f51aa1571d5c548762c11500a2af21b35adb
fionajoyo <2561952525@qq.com>
2021-04-02 12:21:32 +0800

    master

--- Code_C/C_Single/exercise/test.cpp
+++ Code_C/C_Single/exercise/test.cpp
@@ -3,36 +3,8 @@ using namespace std;
 
 
 
-class Increase
-{
-    public: 
-    int value;
-    Increase (){value=0;}
-
-};
-
-Increase operator ++(Increase &a,int)
-{
-  Increase temp;
-  temp.value=a.value++;
-  return temp;
-}
-
-Increase& operator ++(Increase &a)
-
-{
-
-    ++a.value;
-
-    return a;
-
-}
-
-
 
 int main()
 {
-  Increase a;
-  ++a;
-  cout<<++++a.value<<endl;
+ 
 }
